ff4604a8817f615f5a19600f5d9f8e8ba6663aa4
Romchik98 <38857653+Romchik98@users.noreply.github.com>
2022-06-21 21:06:06 +0300

    Not done

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,7 +2,13 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
-
+        if (numberToPrint < 0 && power % 2 == 0) {
+            numberToPrint *= -1;
+        }
+        for (int i = 0; i < power - 1; i++) {
+            numberToPrint *= 10;
+        }
+        System.out.println(numberToPrint);
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,22 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printHalfPyramid(int cathetusLength) {
-
+        for (int i = cathetusLength; i >= 1; i--)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print(" ");
+            }
+            for (int j = i; j <= cathetusLength; j++)
+            {
+                System.out.print(j);
+            }
+            for (int j = cathetusLength - 1; j >= i; j--)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
